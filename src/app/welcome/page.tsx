c01fcd139b304309832bca7f85d08a38cf2c5ec4
Abenezer Esubalw <abenezeresubalwsocial@gmail.com>
2026-05-21 15:05:57 +0000

    Decrease Wave length  (_for element <WelcomePage>_)

--- src/app/welcome/page.tsx
+++ src/app/welcome/page.tsx
@@ -26,8 +26,8 @@ export default function WelcomePage() {
 
   return (
     <main className="min-h-screen relative overflow-hidden bg-[#ffffff]">
-      {/* Animated Red Wave Background on the left side with specific width constraint */}
-      <div className="absolute inset-y-0 left-0 w-full md:w-[calc(40%-100px)] z-0 overflow-hidden pointer-events-none">
+      {/* Animated Red Wave Background on the left side with decreased width/amplitude */}
+      <div className="absolute inset-y-0 left-0 w-full md:w-[calc(35%-100px)] z-0 overflow-hidden pointer-events-none">
         <svg 
           viewBox="0 0 500 1000" 
           className="h-full w-full object-cover" 
@@ -39,9 +39,9 @@ export default function WelcomePage() {
               dur="15s" 
               repeatCount="indefinite"
               values="
-                M0,0 L0,1000 L300,1000 C450,850 150,700 300,500 C450,300 150,150 300,0 Z;
-                M0,0 L0,1000 L400,1000 C250,850 550,700 400,500 C250,300 550,150 400,0 Z;
-                M0,0 L0,1000 L300,1000 C450,850 150,700 300,500 C450,300 150,150 300,0 Z
+                M0,0 L0,1000 L250,1000 C350,850 150,700 250,500 C350,300 150,150 250,0 Z;
+                M0,0 L0,1000 L350,1000 C250,850 450,700 350,500 C250,300 450,150 350,0 Z;
+                M0,0 L0,1000 L250,1000 C350,850 150,700 250,500 C350,300 150,150 250,0 Z
               "
             />
           </path>
