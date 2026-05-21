f96b574a701a03360e79f9198b491e330d6306ee
Abenezer Esubalw <abenezeresubalwsocial@gmail.com>
2026-05-21 13:35:32 +0000

    Red color margin right 75 px (_for element <WelcomePage>_)

--- src/app/welcome/page.tsx
+++ src/app/welcome/page.tsx
@@ -1,4 +1,3 @@
-
 'use client';
 
 import React from 'react';
@@ -24,8 +23,8 @@ export default function WelcomePage() {
 
   return (
     <main className="min-h-screen relative overflow-hidden bg-[#ffffff]">
-      {/* Animated Red Wave Background covering the right half */}
-      <div className="absolute inset-y-0 right-0 w-full md:w-1/2 z-0 overflow-hidden pointer-events-none">
+      {/* Animated Red Wave Background covering the right half with 75px margin */}
+      <div className="absolute inset-y-0 right-0 w-full md:w-1/2 z-0 overflow-hidden pointer-events-none mr-[75px]">
         <svg 
           viewBox="0 0 500 1000" 
           className="h-full w-full object-cover" 
